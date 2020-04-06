9c6f6c9bdf2d400e2bfb450978c4f7386e38d3a9
abhinema <abhinema@gmail.com>
2020-04-06 17:13:19 +0530

    added case for boundry

--- unit_test/src/test_math_logic.c
+++ unit_test/src/test_math_logic.c
@@ -23,6 +23,10 @@ void testAddition1(void){
     TEST_ASSERT_EQUAL_INT(6, addition(3,2));
     TEST_ASSERT_EQUAL_INT(10, addition(3,2));
 }
+void testAdditionboundary(void){
+    TEST_ASSERT_EQUAL_INT(255, addition(126,127));
+    TEST_ASSERT_EQUAL_INT(255, addition(127,127));
+}
 
 void testMultiplication(void){
     TEST_ASSERT_EQUAL_INT(5, multiplication(3,2));
